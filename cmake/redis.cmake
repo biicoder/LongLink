249d0f66ab9f5423c66c297dcaaeddb14331c4de
Bii <156188791@qq.com>
2022-08-18 08:12:52 -0700

    完善redis编译脚本

--- cmake/redis.cmake
+++ cmake/redis.cmake
@@ -1,4 +1,5 @@
 #redis 编译脚本
 
 set(REDIS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/redis)
-add_custom_target(REDIS COMMAND make -C ${REDIS_SOURCE_DIR})
+set(REDIS_INSTALL_DIR ${CMAKE_BINARY_DIR}/third_party/redis)
+add_custom_target(REDIS COMMAND make MALLOC=libc PREFIX=${REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR} && make PREFIX=${REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR} install)
