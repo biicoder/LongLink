f7b476c5e7c4e209575ca5cd40ea89210f47f77c
徐鹏浩 <xupenghao02@meituan.com>
2022-08-19 20:09:56 +0800

    添加redis cpp

--- cmake/redis.cmake
+++ cmake/redis.cmake
@@ -2,4 +2,9 @@
 
 set(REDIS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/redis)
 set(REDIS_INSTALL_DIR ${CMAKE_BINARY_DIR}/third_party/redis)
-add_custom_target(REDIS COMMAND make MALLOC=libc PREFIX=${REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR} && make PREFIX=${REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR} install)
+add_custom_target(REDIS COMMAND make V=1  MALLOC=libc PREFIX=${REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR} && make V=1 PREFIX=${REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR} install)
+
+set(HI_REDIS_INSTALL_DIR ${REDIS_INSTALL_DIR}/hiredis)
+
+add_custom_target(HI_REDIS COMMAND make V=1 PREFIX=${HI_REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR}/deps/hiredis && make  V=1 PREFIX=${HI_REDIS_INSTALL_DIR} -C ${REDIS_SOURCE_DIR}/deps/hiredis install)
+
