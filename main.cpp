ae5506fa02ab35943e1f52c22b564bbc2419b686
徐鹏浩 <xupenghao02@meituan.com>
2022-08-25 12:01:25 +0800

    添加C++类型推导识别工具类

--- main.cpp
+++ main.cpp
@@ -1,8 +1,81 @@
 #include <iostream>
+#include <vector>
+#include <type_traits>
+#include <typeinfo>
+#include <memory>
+#ifndef _MSC_VER
+#include <cxxabi.h>
+#endif
+using namespace std;
 
+template <class T>
+std::string
+type_name()
+{
+    typedef typename std::remove_reference<T>::type TR;
+    std::unique_ptr<char, void(*)(void*)> own
+           (
+#ifndef _MSC_VER
+                abi::__cxa_demangle(typeid(TR).name(), nullptr,
+                                           nullptr, nullptr),
+#else
+                nullptr,
+#endif
+                std::free
+           );
+    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    if (std::is_const<TR>::value)
+        r += " const";
+    if (std::is_volatile<TR>::value)
+        r += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        r += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        r += "&&";
+    return r;
+}
+
+class Obj {
+	public:
+		Obj():m_(0) {
+			cout << "obj construct" << endl;
+		}
+		~Obj() {
+			cout << "obj deconstruct" << endl;
+		}
+		Obj(const Obj& other) {
+			cout << "copy construct" << endl;
+			m_ = other.m_;
+		}
+		Obj(Obj&& other) {
+			cout << "move construct" << endl;
+		}
+	        Obj& operator=(const Obj& other) {
+			cout << "operator =" << endl;
+			return *this;
+		}
+		Obj& operator=(Obj&& other) {
+			cout << "move operator=" << endl;
+			return *this;
+		}
+		int m_;
+};
 int
 main(int argc, char* argv[]) {
 
 	std::cout << "hello world" << std::endl;
+
+	vector<Obj> vec;
+	Obj a;
+	Obj b;
+	Obj c;
+	vec.push_back(a);
+	vec.push_back(b);
+	vec.push_back(c);
+	cout << "-----------" << endl;
+	for(auto it : vec) {
+		cout << type_name<decltype(it)>() << endl;
+		cout << it.m_ << endl;
+	}
 	return 0;
 }
