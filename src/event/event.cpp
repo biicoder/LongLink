96fd6ea77f99e02a9a491ff4091e53efb3f7ec08
Bii <156188791@qq.com>
2022-08-25 08:23:17 -0700

    添加libevent demo

---
+++ src/event/event.cpp
@@ -0,0 +1,18 @@
+#include "event2/event.h"
+#include <iostream>
+#include <signal.h>
+using namespace std;
+static void signal_cb(evutil_socket_t fd, short event, void* arg) {
+  event_base_loopbreak(static_cast<struct event_base*>(arg));
+}
+int
+main() {
+
+	cout << "-------- event demo---------------" << endl;
+	struct event_base* base;
+	struct event* signal_int;
+	base = event_base_new();
+	signal_int = evsignal_new();
+	return 0;
+
+}
